2905b3c2a1f51742bdadab4ec40bbc2ebd577484
qiaomein <jackqiao2002@gmail.com>
2021-07-08 13:39:05 -0500

    generalized bresenham

--- bresenham.cpp
+++ bresenham.cpp
@@ -4,95 +4,128 @@
 #include<time.h>
 #include<string>
 #include<fstream>
+
 using std::string;
 
 float slope(int x1, int y1, int x2, int y2){
     return float((y2-y1))/(x2-x1);
 }
 
-int main () {
-    //file
+
+void bresenham1(int x1, int y1, int x2, int y2) // for slopes <= 1
+{
     std::ofstream myfile;
 
     myfile.open("bresenham.csv");
 
+    int o45 = 0, o8 = 0;
+
+    if (x2-x1<0 && y2-y1>0) {
+        x2 += 2*(x1-x2);
+        o45 = 1;
+        
+    }
+    else if (x2 - x1 < 0 && y2-y1 < 0){
+       x2 += 2*(x1-x2); 
+       y2 += 2*(y1-y2);
+       o45 = 1;
+       o8 = 1;
+    }
+    else if (x2 - x1 > 0 && y2 - y1 < 0){
+       y2 += 2*(y1-y2);
+       o8 = 1;
+    }
+
+   int m_new = 2 * (y2 - y1);
+   int slope_error_new = m_new - (x2 - x1);
+   for (int x = x1, y = y1; x <= x2; x++)
+   {
+      std::cout << "(" << x - o45*(2*(x-x1)) << "," << y - o8*(2*(y-y1)) << ")\n";
+
+      // Add slope to increment angle formed
+      slope_error_new += m_new;
+ 
+      // Slope error reached limit, time to
+      // increment y and update slope error.
+      if (slope_error_new >= 0)
+      {
+         y++;
+         slope_error_new  -= 2 * (x2 - x1);
+      }
+   }
+   myfile.close();
+}
+
+void bresenham2(int x1, int y1, int x2, int y2) //for reflected slopes of > 1
+{
+    std::ofstream myfile;
+
+    myfile.open("bresenham.csv");
+
+   int o36 = 0, o7 = 0;
+
+    if (x2-x1<0 && y2-y1>0) {
+        x2 += 2*(x1-x2);
+        o36 = 1;
+    }
+    else if (x2 - x1 < 0 && y2-y1 < 0){
+       x2 += 2*(x1-x2); 
+       y2 += 2*(y1-y2);
+       o36 = 1;
+       o7 = 1;
+    }
+    else if (x2 - x1 > 0 && y2 - y1 < 0){
+       y2 += 2*(y1-y2);
+       o7 = 1;
+    }
+
+
+   int m_new = 2 * (x2 - x1);
+   int slope_error_new = m_new - (y2 - y1);
+   for (int x = x1, y = y1; y <= y2; y++)
+   {
+      std::cout << "(" << x - o36*(2*(x-x1)) << "," << y - o7*(2*(y-y1)) << ")\n";
+
+      // Add slope to increment angle formed
+      slope_error_new += m_new;
+ 
+      // Slope error reached limit, time to
+      // increment y and update slope error.
+      if (slope_error_new >= 0)
+      {
+         x++;
+         slope_error_new  -= 2 * (y2 - y1);
+      }
+   }
+   myfile.close();
+}
+
+void bresenham(int x1, int y1, int x2, int y2){
+   float m = slope(x1,y1,x2,y2);
+   if (abs(m)<= 1) {
+      return bresenham1(x1,y1,x2,y2);
+   }
+   else{
+      return bresenham2(x1,y1,x2,y2);
+   }
+}
+
+int main () {
+    //file
+    
+
+
     int x0, y0, x1, x2, y1, y2;
 
-    x1 = 0; y1 = 0;
+    std::cout << "Enter x1, y1, x2, y2:" << std::endl;
+    std::cin >> x1 >> y1 >> x2 >> y2;
 
-    std::cout << "Enter x2, y2:" << std::endl;
-    std::cin >> x2 >> y2;
-    float m = slope(x1,y1,x2,y2);
-    x0 = x1;
-    y0 = y1;
-
-    //set chasing boat as the origin
-    x2 = x2-x1;
-    y2 = y2-y1;
-    x1 = 0;
-    y1 = 0;
-    
-    std::cout<<x1<<y1<<x2<<y2<<std::endl;
-
-    int q2 =1; int q3=1; int q4 =1;
-
-    
-
-    //control flow for other quadrants
-    if (x2 < 0 && y2 >0){ // in q2
-        x2 *= -1;
-        q2 = -1;
-    }
-    else if (x2<0 && y2 < 0){
-        x2*=-1; y2 *=-1;
-        q3 = -1;
-    }
-    else if (x2>0 && y2 <0) {
-        y2*=-1;
-        q4 = -1;
-    }
-    std::cout << m << std::endl;
-    if (abs(m) > 1) { //swap x and y if slope > 1
-        std::cout << "m > 1" << std::endl;
-        auto x1temp = x1;
-        x1 = y1; y1 = x1temp;
-        auto x2temp = x2;
-        x2 = y2; y2 = x2temp;
-        auto x0temp = x0; x0 = y0; y0 = x0temp;
-    }
-
-
-//bresenham scheme
-    int m_new = 2 * (y2 - y1);
-    int slope_error_new = m_new - (x2 - x1);
-    //bresneham loop
-    for (int x = x1, y = y1; x <= x2; x++)
-    {
-        
-
-        //control flow for m > 1 and m <= 1
-        if (abs(m)<= 1) {
-        std::cout << "(" << q2*q3*(x+x0) << "," << q3*q4*(y+y0) << ")" << std::endl;
-        myfile << std::to_string(q2*q3*(x+x0)) + "," +std::to_string(q3*q4*(y+y0)) + "\n";
-        }
-        else {
-            std::cout << "(" << (q2*q3*(y+y0)) << "," << q3*q4*(x+x0) << ")" << std::endl;
-        myfile << std::to_string(q2*q3*(y+y0)) + "," +std::to_string(q4*q3*(x+x0)) + "\n";
-        }
-
-        slope_error_new += m_new;
-
-        if (slope_error_new >= 0)
-        {
-            y++;
-            slope_error_new  -= 2 * (x2 - x1);
-        }
-    }
-
+    bresenham(x1,y1,x2,y2);
+   
     
     
 
 
-    myfile.close();
+    
     return 0;
 }
