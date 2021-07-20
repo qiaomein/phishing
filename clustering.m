e601890737d500eba6b784edd355e70390d70346
qiaomein <jackqiao2002@gmail.com>
2021-07-20 01:45:48 -0500

    added threshold.csv

--- clustering.m
+++ clustering.m
@@ -11,10 +11,13 @@ manual(2) = mean(data(:,2));
 manual(3) = mean(data(:,3));
 
 entropies = reshape(data,numel(data),1);
-
-figure;
-plot(entropies,0,'.'); %hold on; plot([0 3.5],[0 0]);
 [idx, C] = kmeans(entropies,3);
+
 manual = sort(manual)
 C = sort(C)
+writematrix([manual C],'thresholds.csv');
+figure;
+plot(entropies,0,'.'); %hold on; plot([0 3.5],[0 0]);
+
+
 hold on; plot(C,0,'.','MarkerSize',30);
