246fff2a4c1db214a231fce40088da3460fa71e7
qiaomein <jackqiao2002@gmail.com>
2022-01-30 14:31:10 -0600

    added voronoi/kmeans detection thresholds

--- plotting.py
+++ plotting.py
@@ -4,7 +4,7 @@ from statistics import *
 import csv
 from matplotlib import pyplot as plt
 #from sklearn.datasets.samples_generator import make_blobs
-#from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans
 
 print('python script ran')
 
@@ -48,23 +48,33 @@ plt.plot(followingwalk,[0]*len(followingwalk),'ro',markersize = 2)
 #plt.xlim(0,4)
 plt.legend(['random','chasing','following'])
 data = [randomwalk,chasingwalk,followingwalk]
+
+DATA = np.array(randomwalk+chasingwalk+followingwalk)
+k_means = KMeans(n_clusters = 3).fit(DATA.reshape(-1,1))
+k_clusters = sorted([float(x) for x in k_means.cluster_centers_])
+print(f"K-means cluster: {k_clusters}")
+
 means = [mean(l) for l in data]
 stdevs = [stdev(l) for l in data]
-print(means)
-print(stdevs)
+print("Supervised means, stdevs: ",means,"|||",stdevs)
 
 plt.plot(means,[0]*3,'yo',markersize=5)
 
 
 ## writing to csv file
 thresholds = [0,means[1]+stdevs[1]*(means[2]-means[1])/(stdevs[2]+stdevs[1]),means[2]+stdevs[2]*(means[0]-means[2])/(stdevs[0]+stdevs[2])]
+voronoi_thresholds = [0,(k_clusters[0]+k_clusters[1])/2,(k_clusters[2]+k_clusters[1])/2]
+
+print("THRESHOLDS:", thresholds); print(voronoi_thresholds)
+
 with open('thresholds.csv','w') as csvfile:
     csvwriter = csv.writer(csvfile)
 
     csvwriter.writerow(thresholds)
 
-for x in thresholds:
-    plt.axvline(x)
+for i in range(len(thresholds)):
+    plt.axvline(thresholds[i])
+    plt.axvline(voronoi_thresholds[i],color = 'red')
 
 plt.subplot(122)
 plt.title('Distributions')
